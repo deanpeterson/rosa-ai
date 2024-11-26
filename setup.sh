a7f8570440c816cfec7ca1f009f4caffa0054265
Daniel Schimpfoessl <dds@redhat.com>
2024-11-26 16:07:24 -0600

    Manage CheCluster in openshift-devspaces

--- setup.sh
+++ setup.sh
@@ -97,6 +97,7 @@ if [[ -n "$step" && "$step" == "6" ]]; then
   __ "Red Hat OpenShift Dev Spaces" 4
   cmd "oc apply -f configs/dev-spaces-subscription.yaml"
   oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/devworkspace-operator.openshift-operators -n openshift-operators 2>/dev/null | grep Succeeded | wc -l"
+  __ "Wait for CheCluster CRD tp be present" 5
   oo 1 "oc get customresourcedefinition checlusters.org.eclipse.che --no-headers=true 2>/dev/null | wc -l"
   __ "Create CheCluster" 4
   cmd "oc apply -f configs/dev-spaces-instance.yaml"
