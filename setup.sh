bc8def934a36a8e0fa6a029a708a5f70650e7166
Daniel Schimpfoessl <dds@redhat.com>
2024-11-26 16:04:25 -0600

    Manage CheCluster in openshift-devspaces

--- setup.sh
+++ setup.sh
@@ -93,12 +93,11 @@ if [[ -n "$step" && "$step" == "6" ]]; then
   cmd oc get DSCInitialization,FeatureTracker -n redhat-ods-operator
   __ "OpenShift Pipelines" 4
   cmd "oc apply -f configs/pipelines-subscription.yaml"
-  sleep 5
-  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators -n openshift-operators | grep Succeeded | wc -l"
+  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/openshift-pipelines-operator-rh.openshift-operators -n openshift-operators 2>/dev/null | grep Succeeded | wc -l"
   __ "Red Hat OpenShift Dev Spaces" 4
   cmd "oc apply -f configs/dev-spaces-subscription.yaml"
-  sleep 5
-  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/devworkspace-operator.openshift-operators -n openshift-operators | grep Succeeded | wc -l"
+  oo 1 "oc get ClusterServiceVersion -l operators.coreos.com/devworkspace-operator.openshift-operators -n openshift-operators 2>/dev/null | grep Succeeded | wc -l"
+  oo 1 "oc get customresourcedefinition checlusters.org.eclipse.che --no-headers=true 2>/dev/null | wc -l"
   __ "Create CheCluster" 4
   cmd "oc apply -f configs/dev-spaces-instance.yaml"
   oo 1 "oc get CheCluster devspaces -n openshift-devspaces -o name --no-headers=true | wc -l"
