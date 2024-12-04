04ec775889c69c2a32768f0d0b2693b9aaf572d1
ROSA User <rosa@bastion.mqds8.internal>
2024-12-04 02:51:32 +0000

    Demo App Automation - adding keycloak steps

--- app.sh
+++ app.sh
@@ -1,21 +1,30 @@
 source tools/format.sh
 step=$1
+clusterName="rosa-$GUID"
 clusterInfo=$(rosa list clusters -o json)
 CONSOLE_URL=$(echo "$clusterInfo" | jq -r '.[].console.url')
+API_URL=$(echo "$clusterInfo" | jq -r '.[].api.url')
 APP_URL=$(echo "$CONSOLE_URL" | cut -d\. -f2-)
-SCRATCH_PATH="$(pwd)/scratch/"
-
+BASE_DOMAIN=$(echo "$CONSOLE_URL" | cut -d\. -f3-)
+SETUP_PATH=$(pwd)
+PROJECT_PATH=$(dirname $SETUP_PATH)
+DEMO_PATH=$PROJECT_PATH/demo-app/
+SCRATCH_PATH="$DEMO_PATH/scratch/"
+GITOPS_PATH="$DEMO_PATH/gitops/"
 __ "Install AI Demo App" 1
 
 if [[ -z "$step" || "$step" == "1" ]]; then 
   __ "Step 1 - Pre-requisites" 2
+
+  __ "Checkout AI Starter Kit" 3
+  cmd "git clone https://github.com/purefield-demo-team/ai-hackathon-starter.git $DEMO_PATH"
   
   __ "Setup Scratch Environment" 3
   cmd "mkdir -p $SCRATCH_PATH"
   
   oc status 2>&1 
   if [ $? -ne 0 ]; then
-  __ "Log into OpenShift Cluster as cluster-admin" 3
+    __ "Log into OpenShift Cluster as cluster-admin" 3
     _? "What is the cluster-admin password" API_PWD $API_PWD
     cmd oc login -u cluster-admin -p "$API_PWD" "$API_URL"
   fi
@@ -41,10 +50,25 @@ fi
 if [[ -n "$step" && "$step" == "3" ]]; then 
   __ "Step 3 - Install Red Hat Keycloak" 2
   __ "Create SSL Certificate" 3
-  cmd "openssl req -subj '/CN=keycloak-${NAMESPACE}.${APP_URL}/O=Test Keycloak./C=US' -newkey rsa:2048 -nodes 
+  __ "Generate SSL Certificate" 4
+  cmd "openssl req -subj '/CN=keycloak-${NAMESPACE}.${APP_URL}/O=Test Keycloak./C=US' 
+        -newkey rsa:2048 -nodes 
         -x509 -days 365 
         -keyout ${SCRATCH_PATH}key.pem
         -out    ${SCRATCH_PATH}certificate.pem"
-  __ "Copy SSL Certificate into secret" 3
+  __ "Copy SSL Certificate into secret" 4
   cmd "oc create secret -n $NAMESPACE tls keycloak-tls-secret --cert ${SCRATCH_PATH}certificate.pem --key ${SCRATCH_PATH}key.pem"
+  __ "Configure postgresql variables" 3
+  postgresqlConfig=${GITOPS_PATH}rhbk/keycloak-postgresql-chart/values.yaml
+  baseDomain=$(echo $BASE_DOMAIN | cut -d\. -f2-)
+  cmd "perl -pe 's/(\s+name:) salamander/\$1 rosa/' -i $postgresqlConfig"
+  cmd "perl -pe 's/(\s+domain:) aiml.*?$/\$1 $baseDomain/' -i $postgresqlConfig"
+  __ "Run helm charts for postgresql" 3
+  cmd "helm install keycloak-postgresql ${GITOPS_PATH}rhbk/keycloak-postgresql-chart/"
+  __ "Use pgAdmin-4 to restore the keycloak.backup file in the gitops/rhbk folder" 3
+    ___ "Continue"
+
+fi
+
+if [[ -n "$step" && "$step" == "4" ]]; then 
 fi
